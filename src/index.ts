265f1cad0a4246bcb803582fc3a0a6578eab7cb8
Robert Soriano <sorianorobertc@gmail.com>
2021-08-09 13:07:03 +0800

    convert createStore to useStore

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { createStore as createSolidStore, reconcile, Store } from 'solid-js/store';
+import { createStore, reconcile, Store } from 'solid-js/store';
 import { onCleanup } from 'solid-js';
 import type { SetState, State, StoreApi } from 'zustand/vanilla';
 
-export default function createStore<T extends State>(store: StoreApi<T>): [Store<T>, SetState<T>] {
-  const [state, setState] = createSolidStore(store.getState());
+export default function useStore<T extends State>(store: StoreApi<T>): [Store<T>, SetState<T>] {
+  const [state, setState] = createStore(store.getState());
   const unsubscribe = store.subscribe((newState) => {
       setState(reconcile(newState));
   });
