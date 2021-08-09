991c30b4ea9d0acc2fab2ee7467d8c7488726bd0
Robert Soriano <sorianorobertc@gmail.com>
2021-08-09 13:06:33 +0800

    default export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { createStore as createSolidStore, reconcile, Store } from 'solid-js/store';
 import { onCleanup } from 'solid-js';
-import { SetState, State, StoreApi } from 'zustand/vanilla';
+import type { SetState, State, StoreApi } from 'zustand/vanilla';
 
-export function createStore<T extends State>(store: StoreApi<T>): [Store<T>, SetState<T>] {
+export default function createStore<T extends State>(store: StoreApi<T>): [Store<T>, SetState<T>] {
   const [state, setState] = createSolidStore(store.getState());
   const unsubscribe = store.subscribe((newState) => {
       setState(reconcile(newState));
