250ebc1b1c527de6602e8dffe4abecd14bd09ddc
Robert Soriano <sorianorobertc@gmail.com>
2021-08-11 23:24:41 +0800

    add shallow state picking

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,21 @@ import createImpl, {
   Subscribe,
   Destroy,
   StateSelector,
+  EqualityChecker,
 } from 'zustand/vanilla';
 
 export interface UseStore<T extends State> {
   (): T;
-  <U>(selector: StateSelector<T, U>): U;
+  <U>(selector: StateSelector<T, U>, equalityFn?: EqualityChecker<U>): U;
   setState: SetState<T>;
   getState: GetState<T>;
   subscribe: Subscribe<T>;
   destroy: Destroy;
 }
+
+export {
+  default as shallow
+} from 'zustand/shallow';
 
 export default function create<TState extends State>(
   createState: StateCreator<TState> | StoreApi<TState>
@@ -27,13 +32,14 @@ export default function create<TState extends State>(
     typeof createState === 'function' ? createImpl(createState) : createState;
 
   const useStore: any = <StateSlice>(
-    selector: StateSelector<TState, StateSlice> = api.getState as any
+    selector: StateSelector<TState, StateSlice> = api.getState as any,
+    equalityFn: EqualityChecker<StateSlice> = Object.is
   ) => {
     const initialValue = selector(api.getState())
     const [state, setState] = createStore(initialValue);
     const unsubscribe = api.subscribe((newState) => {
       setState(reconcile(newState));
-    }, selector);
+    }, selector, equalityFn);
     onCleanup(() => unsubscribe());
     return state;
   }
--- test/index.spec.tsx
+++ test/index.spec.tsx
@@ -1,5 +1,5 @@
 import { render } from 'solid-js/web'
-import create from '../src';
+import create, { shallow } from '../src';
 
 interface BearState {
     bears: number;
@@ -13,14 +13,16 @@ const useStore = create<BearState>(set => ({
     decrease: () => set(state => ({ bears: state.bears - 1 }))
 }));
 
-describe('Testing useStore', () => {
+describe('Default properties', () => {
     it('should include setState, getState, subscribe and destroy properties', () => {
         expect(typeof useStore.setState).toBe('function');
         expect(typeof useStore.getState).toBe('function');
         expect(typeof useStore.subscribe).toBe('function');
         expect(typeof useStore.destroy).toBe('function');
     })
-    
+})
+
+describe('Single state-picks', () => {
     it('should function correct when rendering in Solid', () => {
         const div = document.createElement('div');
         render(() => {
@@ -32,6 +34,27 @@ describe('Testing useStore', () => {
             increase();
             state.decrease();
             return <span>{state.bears}</span>
+        }, div);
+        expect(div.innerHTML).toBe('<span>2</span>');
+    })
+})
+
+describe('Multiple state-picks with shallow', () => {
+    it('should function correct when rendering in Solid', () => {
+        const useStore = create<{ bears: { count: number }, bulls: { count: number } }>(() => ({
+            bears: {
+                count: 0
+            },
+            bulls: {
+                count: 0
+            }
+        }));
+        const div = document.createElement('div');
+        render(() => {
+            const [bears] = useStore(state => [state.bears, state.bulls], shallow)
+            useStore.setState(({ bears }) => ({ bears: { count: bears.count + 1 } }))
+            useStore.setState(({ bears }) => ({ bears: { count: bears.count + 1 } }))
+            return <span>{bears.count}</span>
         }, div);
         expect(div.innerHTML).toBe('<span>2</span>');
     })
