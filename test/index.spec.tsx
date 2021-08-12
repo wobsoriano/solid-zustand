ba75cd2dfd07f6d48e6bda80a74d85e8673bd72f
Robert Soriano <sorianorobertc@gmail.com>
2021-08-12 09:48:35 +0800

    update tests

--- test/index.spec.tsx
+++ test/index.spec.tsx
@@ -13,49 +13,43 @@ const useStore = create<BearState>(set => ({
     decrease: () => set(state => ({ bears: state.bears - 1 }))
 }));
 
-describe('Default properties', () => {
-    it('should include setState, getState, subscribe and destroy properties', () => {
-        expect(typeof useStore.setState).toBe('function');
-        expect(typeof useStore.getState).toBe('function');
-        expect(typeof useStore.subscribe).toBe('function');
-        expect(typeof useStore.destroy).toBe('function');
-    })
+it('should return default zustand properties', () => {
+    expect(typeof useStore.setState).toBe('function');
+    expect(typeof useStore.getState).toBe('function');
+    expect(typeof useStore.subscribe).toBe('function');
+    expect(typeof useStore.destroy).toBe('function');
 })
 
-describe('Single state-picks', () => {
-    it('should function correct when rendering in Solid', () => {
-        const div = document.createElement('div');
-        render(() => {
-            const state = useStore();
-            const increase = useStore(state => state.increase);
-            expect(state.bears).toBe(0);
-            increase();
-            increase();
-            increase();
-            state.decrease();
-            return <span>{state.bears}</span>
-        }, div);
-        expect(div.innerHTML).toBe('<span>2</span>');
-    })
+it('should function correct when rendering in Solid', () => {
+    const div = document.createElement('div');
+    render(() => {
+        const state = useStore();
+        const increase = useStore(state => state.increase);
+        expect(state.bears).toBe(0);
+        increase();
+        increase();
+        increase();
+        state.decrease();
+        return <span>{state.bears}</span>
+    }, div);
+    expect(div.innerHTML).toBe('<span>2</span>');
 })
 
-describe('Multiple state-picks with shallow', () => {
-    it('should function correct when rendering in Solid', () => {
-        const useStore = create<{ bears: { count: number }, bulls: { count: number } }>(() => ({
-            bears: {
-                count: 0
-            },
-            bulls: {
-                count: 0
-            }
-        }));
-        const div = document.createElement('div');
-        render(() => {
-            const [bears] = useStore(state => [state.bears, state.bulls], shallow)
-            useStore.setState(({ bears }) => ({ bears: { count: bears.count + 1 } }))
-            useStore.setState(({ bears }) => ({ bears: { count: bears.count + 1 } }))
-            return <span>{bears.count}</span>
-        }, div);
-        expect(div.innerHTML).toBe('<span>2</span>');
-    })
+it('should allow state multiple state slices', () => {
+    const useStore = create<{ bears: { count: number }, bulls: { count: number } }>(() => ({
+        bears: {
+            count: 0
+        },
+        bulls: {
+            count: 0
+        }
+    }));
+    const div = document.createElement('div');
+    render(() => {
+        const [bears] = useStore(state => [state.bears, state.bulls], shallow)
+        useStore.setState(({ bears }) => ({ bears: { count: bears.count + 1 } }))
+        useStore.setState(({ bears }) => ({ bears: { count: bears.count + 1 } }))
+        return <span>{bears.count}</span>
+    }, div);
+    expect(div.innerHTML).toBe('<span>2</span>');
 })
