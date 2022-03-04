5508e66614fac6edfcb1111fd164782596f1aebc
Robert Soriano <sorianorobertc@gmail.com>
2022-03-04 14:28:19 -0800

    update build tool

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { createStore, reconcile } from 'solid-js/store';
-import { onCleanup } from 'solid-js';
+import { createStore, reconcile } from "solid-js/store"
+import { onCleanup } from "solid-js"
 import createImpl, {
   EqualityChecker,
   GetState,
@@ -8,11 +8,11 @@ import createImpl, {
   StateCreator,
   StateSelector,
   StoreApi,
-} from 'zustand/vanilla';
+} from "zustand/vanilla"
 
-export type UseBoundStore<
+type UseBoundStore<
   T extends State,
-  CustomStoreApi extends StoreApi<T> = StoreApi<T>
+  CustomStoreApi extends StoreApi<T> = StoreApi<T>,
 > = {
   (): T
   <U>(selector: StateSelector<T, U>, equalityFn?: EqualityChecker<U>): U
@@ -22,41 +22,42 @@ export default function create<
   TState extends State,
   CustomSetState = SetState<TState>,
   CustomGetState = GetState<TState>,
-  CustomStoreApi extends StoreApi<TState> = StoreApi<TState>
+  CustomStoreApi extends StoreApi<TState> = StoreApi<TState>,
 >(
   createState:
     | StateCreator<TState, CustomSetState, CustomGetState, CustomStoreApi>
-    | CustomStoreApi
+    | CustomStoreApi,
 ): UseBoundStore<TState, CustomStoreApi> {
   const api: StoreApi<TState> =
-    typeof createState === 'function' ? createImpl(createState) : createState;
+    typeof createState === "function" ? createImpl(createState) : createState
 
   const useStore: any = <StateSlice>(
     selector: StateSelector<TState, StateSlice> = api.getState as any,
-    equalityFn: EqualityChecker<StateSlice> = Object.is
+    equalityFn: EqualityChecker<StateSlice> = Object.is,
   ) => {
-    const initialValue = selector(api.getState());
-    const [state, setState] = createStore(initialValue);
-    
+    const initialValue = selector(api.getState())
+    const [state, setState] = createStore(initialValue)
+
     const listener = () => {
-      const nextState = api.getState();
-      const nextStateSlice = selector(nextState);
+      const nextState = api.getState()
+      const nextStateSlice = selector(nextState)
 
       try {
+        // @ts-ignore
         if (!equalityFn(state, nextStateSlice)) {
-          setState(reconcile(nextStateSlice));
+          setState(reconcile(nextStateSlice))
         }
       } catch (e) {
-        setState(reconcile(nextStateSlice));
+        setState(reconcile(nextStateSlice))
       }
     }
 
-    const unsubscribe = api.subscribe(listener);
-    onCleanup(() => unsubscribe());
-    return state;
+    const unsubscribe = api.subscribe(listener)
+    onCleanup(() => unsubscribe())
+    return state
   }
 
-  Object.assign(useStore, api);
+  Object.assign(useStore, api)
 
-  return useStore;
+  return useStore
 }
---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitest/config";
+import solid from "vite-plugin-solid";
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    transformMode: {
+      web: [/.[jt]sx?/],
+    },
+    deps: {
+      inline: [/solid-js/],
+    },
+    threads: false,
+    isolate: false,
+  },
+  plugins: [solid()],
+  resolve: {
+    conditions: ["development", "browser"],
+  },
+});
