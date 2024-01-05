0ccf0ee6157bcf03600400a38cce89fe2eb12f03
wobsoriano <sorianorobertc@gmail.com>
2024-01-05 14:39:06 -0800

    remove tsx

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,7 +7,7 @@ const preset_options: preset.PresetOptions = {
     // default entry (index)
     {
       // entries with '.tsx' extension will have `solid` export condition generated
-      entry: 'src/index.tsx',
+      entry: 'src/index.ts',
       // will generate a separate development entry
       dev_entry: true,
     },
